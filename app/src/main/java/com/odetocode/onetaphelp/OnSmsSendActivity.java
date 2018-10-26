d1343d6f513e3aee3269c1f8c6da9faff9c38eba
TanavShah <tshah@cs.iitr.ac.in>
2018-10-26 19:36:18 +0530

    onsmssenddone

--- app/src/main/java/com/odetocode/onetaphelp/OnSmsSendActivity.java
+++ app/src/main/java/com/odetocode/onetaphelp/OnSmsSendActivity.java
@@ -1,7 +1,10 @@
 package com.odetocode.onetaphelp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OnSmsSendActivity extends AppCompatActivity
 {
@@ -11,5 +14,14 @@ public class OnSmsSendActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_sms_send);
+        Button bt =
+        findViewById(R.id.button2);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OnSmsSendActivity.this,StartPageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
